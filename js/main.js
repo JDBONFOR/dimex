c781e34209b92061d4ef9be39766480f0cdb34db
juanbonforti <juan.bonforti@gmail.com>
2017-05-01 11:18:21 -0300

    dimex_v0.1

--- js/main.js
+++ js/main.js
@@ -5,23 +5,6 @@ $(document).ready(function() {
     $('.dropdown').on('hide.bs.dropdown', function(e) {
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
     });
-
-    /*
-    $('.salud-area').on('click', function(e) {
-        if ($(window).width() > 479) {
-            if ($('.salud-area-extra').is(":hidden")) {
-                $('.industrial-area-extra').stop(true, true).slideUp();
-                $('.salud-area-extra').stop(true, true).slideDown();
-            } else {
-                $('.salud-area-extra').stop(true, true).slideUp();
-            }
-        } else if ($('.mobile').is(":hidden")) {
-            $('.mobile').stop(true, true).slideDown();
-            $('.industrial-area-extra').stop(true, true).slideUp();
-        } else {
-            $('.mobile').stop(true, true).slideUp();
-        }
-    });*/
     $('.salud-area').on('click', function(e) {
         if ($('.salud-area-extra').is(":hidden")) {
             $('.industrial-area-extra').stop(true, true).slideUp();
@@ -33,7 +16,6 @@ $(document).ready(function() {
             $('.salud-area-extra').stop(true, true).slideUp();
         }
     });
-
     $('.industrial-area').on('click', function(e) {
         if ($('.industrial-area-extra').is(":hidden")) {
             $('.industrial-area-extra').stop(true, true).slideDown();
